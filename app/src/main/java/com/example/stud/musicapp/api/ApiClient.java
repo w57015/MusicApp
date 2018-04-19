f43417ab7f4ad9dc0dd9015bf15ecec0850971ac
W57015 <w57015@student.wsiz.rzeszow.pl>
2018-04-19 17:57:59 +0200

    api

---
+++ app/src/main/java/com/example/stud/musicapp/api/ApiClient.java
@@ -0,0 +1,8 @@
+package com.example.stud.musicapp.api;
+
+/**
+ * Created by W57015 on 2018-04-19.
+ */
+
+public interface ApiClient {
+}
---
+++ app/src/main/java/com/example/stud/musicapp/api/ApiService.java
@@ -0,0 +1,19 @@
+package com.example.stud.musicapp.api;
+
+import retrofit2.Retrofit;
+
+/**
+ * Created by W57015 on 2018-04-19.
+ */
+
+public class ApiService {
+
+    public static ApiClient getService() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://www.theaudiodb.com/api/v1/json/{APIKEY}/")
+                .build();
+
+        return retrofit.create(ApiClient.class);
+    }
+
+}
