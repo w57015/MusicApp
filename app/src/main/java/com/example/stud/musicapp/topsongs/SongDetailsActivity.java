98677a104f454d5326b02b12f47b4a410be10b05
W57015 <w57015@student.wsiz.rzeszow.pl>
2018-05-17 18:33:23 +0200

    Akcja ulubione

--- app/src/main/java/com/example/stud/musicapp/topsongs/SongDetailsActivity.java
+++ app/src/main/java/com/example/stud/musicapp/topsongs/SongDetailsActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -96,9 +97,27 @@ public class SongDetailsActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu. favorite_menu , menu);
+        inflater.inflate(R.menu.favorite_menu, menu);
         return true;
 
     }
 
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id. itemFavorite :
+                addRemoveFavorite();
+                return true ;
+            default :
+                return super .onOptionsItemSelected(item);
+        }
+
+
+    }
+
+    private void addRemoveFavorite() {
+        Toast.makeText(this, "TODO", Toast.LENGTH_SHORT).show();
+    }
 }
