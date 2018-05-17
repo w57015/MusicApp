3ba14529d5a9e08b029175cc28b96ec37d53c0ba
W57015 <w57015@student.wsiz.rzeszow.pl>
2018-05-17 19:16:18 +0200

    Dodawanie/usuwanie z ulubionych

--- app/src/main/java/com/example/stud/musicapp/topsongs/SongDetailsActivity.java
+++ app/src/main/java/com/example/stud/musicapp/topsongs/SongDetailsActivity.java
@@ -16,7 +16,11 @@ import com.example.stud.musicapp.R;
 import com.example.stud.musicapp.api.ApiService;
 import com.example.stud.musicapp.api.Track;
 import com.example.stud.musicapp.api.Tracks;
+import com.example.stud.musicapp.database.Favorite;
 
+import java.util.Date;
+
+import io.realm.Realm;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -29,6 +33,10 @@ public class SongDetailsActivity extends AppCompatActivity {
     public static final String ARTIST = "artist" ;
     public static final String TRACK_ID = "track_id" ;
 
+    String track;
+    String artist;
+    int trackId;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -40,9 +48,9 @@ public class SongDetailsActivity extends AppCompatActivity {
         getSupportActionBar().setSubtitle(ARTIST);
 
         Intent intent = getIntent();
-        String track = intent.getStringExtra(TRACK);
-        String artist = intent.getStringExtra(ARTIST);
-        int trackId = intent.getIntExtra(TRACK_ID, 0);
+         track = intent.getStringExtra(TRACK);
+         artist = intent.getStringExtra(ARTIST);
+         trackId = intent.getIntExtra(TRACK_ID, 0);
 
         getSupportActionBar().setTitle(track);
         getSupportActionBar().setSubtitle(artist);
@@ -118,6 +126,41 @@ public class SongDetailsActivity extends AppCompatActivity {
     }
 
     private void addRemoveFavorite() {
-        Toast.makeText(this, "TODO", Toast.LENGTH_SHORT).show();
+        Realm realm = Realm. getDefaultInstance ();
+        Favorite favorite = realm
+                .where(Favorite. class )
+                .equalTo( "trackId" , trackId )
+                .findFirst();
+        if (favorite == null ) {
+            addToFavorites(realm);
+        } else {
+            removeFromFavorites(realm, favorite);
+        }
+    }
+    private void addToFavorites(Realm realm) {
+        realm.executeTransaction( new Realm.Transaction() {
+            @Override
+            public void execute( @NonNull Realm realm) {
+                Favorite favorite = realm.createObject(Favorite. class );
+                favorite.setArtist( artist );
+                favorite.setTrack( track );
+                favorite.setTrackId( trackId );
+                favorite.setDate( new Date());
+                Toast. makeText (SongDetailsActivity. this , "Dodano do ulubionych" ,
+                        Toast. LENGTH_SHORT ).show();
+            }
+        });
+
+
+    }
+    private void removeFromFavorites(Realm realm, final Favorite favorite) {
+        realm.executeTransaction( new Realm.Transaction() {
+            @Override
+            public void execute( @NonNull Realm realm) {
+                favorite .deleteFromRealm();
+                Toast. makeText (SongDetailsActivity. this , "Usunięto z ulubionych" ,
+                        Toast. LENGTH_SHORT ).show();
+            }
+        });
     }
 }
